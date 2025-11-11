2f369a64b43f3f604d89272030aa5fa648a8fd46
JESUSHMSIS <jeshuanaco7@gmail.com>
2025-11-11 18:47:24 -0400

    add secuency and times in my motion.div

--- components/BasicsOfMotion.tsx
+++ components/BasicsOfMotion.tsx
@@ -9,23 +9,32 @@ const BasicsOfMotion = () => {
     <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
       <motion.button
         onClick={() => setInvisible(!invisible)}
+        layout
         className="bg-blue-800 text-white uppercase p-2 rounded-2xl"
       >
         {invisible ? (
-          <motion.p initial={{ rotate: "0deg" }} animate={{ rotate: "360deg" }}>
+          <motion.p
+            initial={{ rotate: "0deg" }}
+            animate={{ rotate: "360deg" }}
+            exit={{ rotate: "0deg" }}
+          >
             Ocultar
           </motion.p>
         ) : (
           <motion.p>Mostrar</motion.p>
         )}
       </motion.button>
-      <AnimatePresence>
+      <AnimatePresence mode="popLayout">
         {invisible && (
           <motion.div
-            initial={{ rotate: "0deg", scale: 0 }}
-            animate={{ rotate: "180deg", scale: 1 }}
-            exit={{ rotate: "0deg", scale: 0 }}
-            transition={{ duration: 0.4, ease: "backInOut" }}
+            initial={{ rotate: "0deg", scale: 0, y: 0 }}
+            animate={{ rotate: "180deg", scale: 1, y: [0, 150, -150, -150, 0] }} //en y:[] <- significa la secuencia que seguira una vez que aparezca el div
+            exit={{ rotate: "0deg", scale: 0, y: 0 }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
+              times: [0, 0.25, 0.5, 0.7, 1], //<- aca estan los tiempos para cada secuencia de la transicion que llevara el div
+            }}
             className="bg-white w-[100px] h-[100px]"
           ></motion.div>
         )}
