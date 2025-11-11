a8f6a02ec34a37d269888658115650d54f63fdec
JESUSHMSIS <jeshuanaco7@gmail.com>
2025-11-11 18:34:31 -0400

    add conditional animate with motion

--- components/BasicsOfMotion.tsx
+++ components/BasicsOfMotion.tsx
@@ -1,16 +1,35 @@
 "use client";
 
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
+import { useState } from "react";
 
 const BasicsOfMotion = () => {
+  const [invisible, setInvisible] = useState(true);
   return (
     <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
-      <motion.div
-        initial={{ rotate: "0deg" }}
-        animate={{ rotate: "180deg" }}
-        transition={{ duration: 1 }}
-        className="bg-white w-[100px] h-[100px]"
-      ></motion.div>
+      <motion.button
+        onClick={() => setInvisible(!invisible)}
+        className="bg-blue-800 text-white uppercase p-2 rounded-2xl"
+      >
+        {invisible ? (
+          <motion.p initial={{ rotate: "0deg" }} animate={{ rotate: "360deg" }}>
+            Ocultar
+          </motion.p>
+        ) : (
+          <motion.p>Mostrar</motion.p>
+        )}
+      </motion.button>
+      <AnimatePresence>
+        {invisible && (
+          <motion.div
+            initial={{ rotate: "0deg", scale: 0 }}
+            animate={{ rotate: "180deg", scale: 1 }}
+            exit={{ rotate: "0deg", scale: 0 }}
+            transition={{ duration: 0.4, ease: "backInOut" }}
+            className="bg-white w-[100px] h-[100px]"
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
